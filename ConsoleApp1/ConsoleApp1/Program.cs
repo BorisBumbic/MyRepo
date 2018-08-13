465a3820ee7e6839c488c504f5ace0ce309887a2
Boris Bumbic <boris.bumbic@gmail.com>
2018-08-13 15:05:30 +0200

    äntligen klar

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a command");
+
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine("Enter your preferred character");
+
+            string pref = Console.ReadLine(); ;
+
+            string[] userInputArray = userInput.Split();
+
+            foreach (var item in userInputArray)
+            {
+                int temp = int.Parse(item);
+
+                for (int i = 0; i < temp; i++)
+                {
+                    Console.Write(pref);
+
+                    for (int j = 0; j < temp; j++)
+                    {
+                        Console.Write(pref);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
