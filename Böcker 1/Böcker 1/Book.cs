762bf46c8942ed12c50c8f276264e47f2a34e1ff
Boris Bumbic <boris.bumbic@gmail.com>
2018-08-14 10:12:43 +0200

    Övning i att skapa klasser, metoder samt användandert av regex.

---
+++ Böcker 1/Böcker 1/Book.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Böcker_1
+{
+    class Book
+    {
+        string authorName;
+        string ISBN;
+        int numberOfPages;
+        
+   
+
+        public void SetAuthorName(string name)
+        {
+            authorName = name;
+        }
+
+        public string GetAuthorName()
+        {
+            return authorName;
+        }
+
+        public void SetISBN(string number)
+        {
+            if (Regex.IsMatch(number, @"\d\d\d-\d-\d\d-\d\d\d\d\d\d-\d$"))
+            ISBN = number;
+        }
+
+        public string GetISBN()
+        {
+            return ISBN;
+        }
+
+        public void SetNumberOfPages(int pageNumbers)
+        {
+            numberOfPages = pageNumbers;
+
+            if (numberOfPages > 1000)
+                numberOfPages = 300;
+        }
+
+        public int GetNumberOfPages()
+        {
+            return numberOfPages;
+        }
+
+        public string BookThicknessCalculation()
+        {
+            if (numberOfPages < 100)
+                return "Tunn";
+            else if (numberOfPages > 100 && numberOfPages < 300)
+                return "Normal";
+            else
+                return "Tjock";
+        }
+
+        public double WeightInGrams()
+        {
+            double bookWeight = numberOfPages * 0.8;
+            return bookWeight;
+        }
+
+    }
+}
+
+
---
+++ Böcker 1/Böcker 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Böcker_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Console.WriteLine("Skriv in författarens namn.");
+            book.SetAuthorName(Console.ReadLine());
+            Console.WriteLine("Skriv in bokens ISBN nummer. Formatet är 000-0-00-000000-0");
+            book.SetISBN(Console.ReadLine());
+            Console.WriteLine("Skriv in hur många sidor boken har.");
+            book.SetNumberOfPages(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"Info om boken:");
+            Console.WriteLine();
+            Console.WriteLine($"   ISBN:        {book.GetISBN()}");
+            Console.WriteLine($"   Författare:  {book.GetAuthorName()}");
+            Console.WriteLine($"   Antal sidor: {book.GetNumberOfPages()}");
+            Console.WriteLine($"   Vikt:        {book.WeightInGrams()} gram");
+            Console.WriteLine($"   Storlek:     {book.BookThicknessCalculation()}");
+            Console.WriteLine();
+
+        }
+    }
+}
