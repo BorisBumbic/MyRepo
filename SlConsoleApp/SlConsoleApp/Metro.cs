530545b0473febb615c5ce4666e812f2adbb4b8e
Boris Bumbic <boris.bumbic@gmail.com>
2018-08-23 15:25:08 +0200

    no message

--- SlConsoleApp/SlConsoleApp/Metro.cs
+++ SlConsoleApp/SlConsoleApp/Metro.cs
@@ -6,21 +6,80 @@ using System.Threading.Tasks;
 
 namespace SlConsoleApp
 {
-    class Metro
+
+    public class Metro
     {
-        public string GroupOfLine { get => GroupOfLine;  set  => GroupOfLine = value; }
-        public string DisplayTime { get => DisplayTime;  set  => DisplayTime = value; }
-        public string TransportMode { get => TransportMode;  set  => TransportMode = value; }
-        public int LineNumber { get => LineNumber;  set  => LineNumber = value; }
-        public string Destination { get => Destination;  set  => Destination = value; }
-        public int JourneyDirection { get => JourneyDirection;  set  => JourneyDirection = value; }
-        public string StopAreaName { get => StopAreaName;  set  => StopAreaName = value; }
-        public int StopAreaNumber { get => StopAreaNumber;  set  => StopAreaNumber = value; }
-        public int StopPointNumber { get => StopPointNumber;  set  => StopPointNumber = value; }
-        public string StopPointDesignation { get => StopPointDesignation;  set  => StopPointDesignation = value; }
-        public DateTime TimeTableDateTime { get => TimeTableDateTime;  set  => TimeTableDateTime = value; }
-        public DateTime ExpectedDateTime { get => ExpectedDateTime;  set  => ExpectedDateTime = value; }
-        public int JourneyNumber { get => JourneyNumber;  set  => JourneyNumber = value; }
-        public string Deviation { get => Deviation;  set  => Deviation = value; }
+        public string GroupOfLine { get; set; }
+        public string DisplayTime { get; set; }
+        public string TransportMode { get; set; }
+        public string LineNumber { get; set; }
+        public string Destination { get; set; }
+        public int JourneyDirection { get; set; }
+        public string StopAreaName { get; set; }
+        public int StopAreaNumber { get; set; }
+        public int StopPointNumber { get; set; }
+        public string StopPointDesignation { get; set; }
+        public DateTime TimeTabledDateTime { get; set; }
+        public DateTime ExpectedDateTime { get; set; }
+        public int JourneyNumber { get; set; }
+        public object Deviations { get; set; }
     }
+
+    public class ResponseData
+    {
+        public DateTime LatestUpdate { get; set; }
+        public List<Metro> Metros { get; set; }
+    }
+
+    public class RootObject
+    {
+        public ResponseData ResponseData { get; set; }
+    }
+
+
+
+    //class Metro
+    //{
+    //    //    public string GroupOfLine { get => GroupOfLine;  set  => GroupOfLine = value; }
+    //    //    public string DisplayTime { get => DisplayTime;  set  => DisplayTime = value; }
+    //    //    public string TransportMode { get => TransportMode;  set  => TransportMode = value; }
+    //    //    public int LineNumber { get => LineNumber;  set  => LineNumber = value; }
+    //    //    public string Destination { get => Destination;  set  => Destination = value; }
+    //    //    public int JourneyDirection { get => JourneyDirection;  set  => JourneyDirection = value; }
+    //    //    public string StopAreaName { get => StopAreaName;  set  => StopAreaName = value; }
+    //    //    public int StopAreaNumber { get => StopAreaNumber;  set  => StopAreaNumber = value; }
+    //    //    public int StopPointNumber { get => StopPointNumber;  set  => StopPointNumber = value; }
+    //    //    public string StopPointDesignation { get => StopPointDesignation;  set  => StopPointDesignation = value; }
+    //    //    public DateTime TimeTableDateTime { get => TimeTableDateTime;  set  => TimeTableDateTime = value; }
+    //    //    public DateTime ExpectedDateTime { get => ExpectedDateTime;  set  => ExpectedDateTime = value; }
+    //    //    public int JourneyNumber { get => JourneyNumber;  set  => JourneyNumber = value; }
+    //    //    public string Deviation { get => Deviation;  set  => Deviation = value; }
+
+    //    public string GroupOfLine { get; set; }
+    //    public string DisplayTime { get; set; }
+    //    public string TransportMode { get; set; }
+    //    public string LineNumber { get; set; }
+    //    public string Destination { get; set; }
+    //    public int JourneyDirection { get; set; }
+    //    public string StopAreaName { get; set; }
+    //    public int StopAreaNumber { get; set; }
+    //    public int StopPointNumber { get; set; }
+    //    public string StopPointDesignation { get; set; }
+    //    public DateTime TimeTabledDateTime { get; set; }
+    //    public DateTime ExpectedDateTime { get; set; }
+    //    public int JourneyNumber { get; set; }
+    //    public object Deviations { get; set; }
+
+
+    //    public class ResponseData
+    //    {
+    //        public DateTime LatestUpdate { get; set; }
+    //        public List<Metro> Metros { get; set; }
+    //    }
+
+    //    public class RootObject
+    //    {
+    //        public ResponseData ResponseData { get; set; }
+    //    }
+    //}
 }
--- SlConsoleApp/SlConsoleApp/Program.cs
+++ SlConsoleApp/SlConsoleApp/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SlConsoleApp
@@ -11,13 +14,72 @@ namespace SlConsoleApp
     {
         static void Main(string[] args)
         {
-            using (var webClient = new WebClient()) 
+            using (var webClient = new HttpClient()) 
             {
-               byte[] data = webClient.DownloadData("http://api.sl.se/api2/realtimedeparturesV4.json?key=fa7f8eaa090b4d6791ae9eda0f4384db&siteid=9192&timewindow=0");
+                //byte[] data = webClient.DownloadData("http://api.sl.se/api2/realtimedeparturesV4.json?key=fa7f8eaa090b4d6791ae9eda0f4384db&siteid=9192&timewindow=0");
 
-                JsonConvert.DeserializeObject<MetroCollection>
 
+                string content = webClient.GetAsync("http://api.sl.se/api2/realtimedeparturesV4.json?key=fa7f8eaa090b4d6791ae9eda0f4384db&siteid=9192&timewindow=0").Result.Content.ReadAsStringAsync().Result;
+
+                var x = JsonConvert.DeserializeObject<RootObject>(content) ;
+
+                var periferi = new List<Metro>();
+                var central = new List<Metro>();
+                foreach (var metro in x.ResponseData.Metros)
+                {
+                    if (metro.Destination != "Hässelby strand") 
+                            central.Add(metro);
+                    else
+                        periferi.Add(metro); 
+
+                }
+
+                
+                    Console.WriteLine($"{central[0].Destination}  {central[0].ExpectedDateTime} *** {periferi[0].Destination}  {periferi[0].ExpectedDateTime}");
+
+                
             }
         }
+        //private void Metro_Load(object x, EventArgs e)
+        //{
+        //    Timer timer = new Timer();
+        //    timer.Interval = (60 * 1000);
+        //    timer.Tick += new EventHandler(timer_Tick);
+        //    timer.Start();
+        //}
+
+        //private void timer_Tick(object sender, EventArgs e)
+        //{
+        //    RefreshControls();
+        //}
     }
 }
+
+/*
+ {
+ 
+    "ResponseData": {
+        "LatestUpdate": "2018-08-23T13:58:45",
+
+        "Metros": [
+            {
+                "GroupOfLine": "tunnelbanans gröna linje",
+                "DisplayTime": "Nu",
+                "TransportMode": "METRO",
+                "LineNumber": "18",
+                "Destination": "Alvik",
+                "JourneyDirection": 1,
+                "StopAreaName": "Slussen",
+                "StopAreaNumber": 1011,
+                "StopPointNumber": 1011,
+                "StopPointDesignation": "1",
+                "TimeTabledDateTime": "2018-08-23T13:59:00",
+                "ExpectedDateTime": "2018-08-23T13:59:28",
+                "JourneyNumber": 10319,
+                "Deviations": null
+            },
+            ]
+            }
+            }
+     
+     */
