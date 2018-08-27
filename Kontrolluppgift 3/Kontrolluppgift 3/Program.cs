0875b29a67d9809c5b2976e8985863e749511b1c
Boris Bumbic <boris.bumbic@gmail.com>
2018-08-27 10:29:50 +0200

    sl konsollapp

---
+++ Kontrolluppgift 3/Kontrolluppgift 3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontrolluppgift_3
+{
+    class Program
+    {
+        public static List<int> MultipleBy100AndAdd3(List<int> numbers)
+        {
+            var returnList = new List<int>();
+
+            foreach (var item in numbers)
+            {
+                int temp = item * 100 + 3;
+                returnList.Add(temp);
+            }
+            return returnList;
+        }
+
+        public static List<string> ReorderList(List<string> input, List<int> numbers)
+        {
+            var returnList = new List<string>();
+
+            foreach (var item in numbers)
+            {
+                string temp = input[item-1];
+                returnList.Add(temp);
+            }
+            return returnList;
+        }
+
+        static void Main(string[] args)
+        {
+            var testList = new List<int> { 1, 2, 4, 3 };
+            var stringList = new List<string> { "a", "b", "c", "d" };
+
+            //var outList = MultipleBy100AndAdd3(testList);
+
+            var outList = ReorderList(stringList,testList);
+
+            foreach (var item in outList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- SlConsoleApp/SlConsoleApp/Program.cs
+++ SlConsoleApp/SlConsoleApp/Program.cs
@@ -1,12 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+using System.Timers;
+
 
 namespace SlConsoleApp
 {
@@ -14,89 +12,75 @@ namespace SlConsoleApp
     {
         static void Main(string[] args)
         {
-            using (var webClient = new HttpClient()) 
-            {
-                //byte[] data = webClient.DownloadData("http://api.sl.se/api2/realtimedeparturesV4.json?key=fa7f8eaa090b4d6791ae9eda0f4384db&siteid=9192&timewindow=0");
+            Metro_Load();
+            Console.ReadKey();
+        }
+        static void Metro_Load()
+        {
+            System.Timers.Timer timer = new System.Timers.Timer();
+            Console.Clear();
+            timer.Enabled = true;
+            timer.Elapsed += timer_Tick;
+            timer.Interval = 5000;
+            timer.AutoReset = true;
+            getSLdata();
 
+        }
+
+        static void timer_Tick(Object source, ElapsedEventArgs e)
+        {
+            getSLdata();   
+        }
+
+        static void getSLdata()
+        {
+            using (var webClient = new HttpClient())
+            {
+                Console.SetCursorPosition(0, 0);
+                
 
                 string content = webClient.GetAsync("http://api.sl.se/api2/realtimedeparturesV4.json?key=fa7f8eaa090b4d6791ae9eda0f4384db&siteid=9302&timewindow=0").Result.Content.ReadAsStringAsync().Result;
 
-                var x = JsonConvert.DeserializeObject<RootObject>(content) ;
+                var x = JsonConvert.DeserializeObject<RootObject>(content);
 
+                //För att kunna skilja på tåg som åker mot- och bort från T-centralen gör vi nedan två listor.
                 var periferi = new List<Metro>();
                 var central = new List<Metro>();
 
+                //I denna foreach-loop så går vi igenom all data vi hämtat ifrån Trafiklab och delar upp den så att rätt avgångar hamnar i rätt lista.
+                //Kriteriet för vår uppdelning är ifall avgångens destination är Akalla eller inte.
                 foreach (var metro in x.ResponseData.Metros)
                 {
-                    if (metro.Destination != "Akalla") 
-                            central.Add(metro);
+                    if (metro.Destination != "Akalla")
+                        central.Add(metro);
                     else
-                        periferi.Add(metro); 
+                        periferi.Add(metro);
 
                 }
 
+                //För att for-loopen nedan inte ska generera ett "OutOfRangeException" så skapar vi två ints. De kommer att jämföras i for-loopen och den
+                //med lägre värde kommer att användas som antalet gånger loopen skall köras.
                 int len1 = central.Count;
                 int len2 = periferi.Count;
-                int little = len1;
-                if (little < len2)
-                    little = len2;
 
+                //Nedan deklarerar vi variabeln 'time' som är av typen DateTime. I den sparar vi den exakt tidpunkten informationen mottogs ifrån Trafiklab.
                 DateTime time = x.ResponseData.LatestUpdate;
+
+                //I  utskriftsdelen av vår kod så är den intressantaste delen användandet av 'positioneringsvariabler'. Exempel: {central[i].Destination, -5}. 
                 Console.WriteLine("------------------------------------------------");
                 Console.WriteLine($"--Kista Tunnelbanestation--{time}--");
 
 
-                Console.WriteLine("------------------------------------------------\n************************************************\n***Mot T-Centralen*************Mot Akalla*******\n************************************************");
+                Console.WriteLine("------------------------------------------------\n************************************************\n** Mot T-Centralen *********** Mot Akalla ******\n************************************************");
 
 
-                for (int i = 0; i < len2; i++)
+                for (int i = 0; i < Math.Min(len1, len2); i++)
                 {
-                    Console.WriteLine($"**{central[i].Destination}  {central[i].DisplayTime} *** {periferi[i].Destination}  {periferi[i].DisplayTime}");
-
+                    Console.WriteLine(String.Format($"** {central[i].Destination.PadRight(5)} {central[i].DisplayTime.PadRight(6)} *** {periferi[i].Destination.PadRight(5)} {periferi[i].DisplayTime.PadRight(8)} **"));
                 }
                 Console.WriteLine("************************************************\n************************************************");
-            }
+            };
         }
-        //private void Metro_Load(object x, EventArgs e)
-        //{
-        //    Timer timer = new Timer();
-        //    timer.Interval = (60 * 1000);
-        //    timer.Tick += new EventHandler(timer_Tick);
-        //    timer.Start();
-        //}
-
-        //private void timer_Tick(object sender, EventArgs e)
-        //{
-        //    RefreshControls();
-        //}
     }
 }
 
-/*
- {
- 
-    "ResponseData": {
-        "LatestUpdate": "2018-08-23T13:58:45",
-
-        "Metros": [
-            {
-                "GroupOfLine": "tunnelbanans gröna linje",
-                "DisplayTime": "Nu",
-                "TransportMode": "METRO",
-                "LineNumber": "18",
-                "Destination": "Alvik",
-                "JourneyDirection": 1,
-                "StopAreaName": "Slussen",
-                "StopAreaNumber": 1011,
-                "StopPointNumber": 1011,
-                "StopPointDesignation": "1",
-                "TimeTabledDateTime": "2018-08-23T13:59:00",
-                "ExpectedDateTime": "2018-08-23T13:59:28",
-                "JourneyNumber": 10319,
-                "Deviations": null
-            },
-            ]
-            }
-            }
-     
-     */
