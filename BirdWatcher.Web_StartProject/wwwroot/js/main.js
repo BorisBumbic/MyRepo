db02bc5529328e7908ca3faab8f2ec5b5ac8b080
Boris Bumbic <boris.bumbic@gmail.com>
2018-09-30 16:13:06 +0200

    checkpoint

---
+++ BirdWatcher.Web_StartProject/wwwroot/js/main.js
@@ -0,0 +1,61 @@
+﻿
+async function recreateDatabase() {
+    document.getElementById("recreateButton").style.display = "none";
+    document.body.style.backgroundColor = "blue";
+
+    let response = await fetch("/observation/recreate", {
+        method: "POST"
+    });
+
+    if (response.status === 200) {
+        document.getElementById("recreateButton").style.display = "block";
+        document.body.style.backgroundColor = "green";
+
+    } else {
+        document.getElementById("recreateButton").style.display = "block";
+        document.body.style.backgroundColor = "red";
+
+    }
+
+}
+
+async function addToDb() {
+
+    var response = await fetch("/observation/addy",
+        {
+            method: "POST",
+
+            body: JSON.stringify({
+                BirdName: document.getElementById("BirdName").value
+            }),
+
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+    if (response.status === 200) {
+        console.log(`Bird added!`)
+    }
+    else {
+        console.log("Something didnt work out!")
+    }
+}
+
+async function displayObservations() {
+    var response = await fetch("/observation", { method: "GET" })
+
+}
+
+
+//if (response.status === 200)
+//    let allBirds = await response.json();
+
+//let html = "";
+
+//for (let bird in allBirds) {
+
+//    html = bird + "<br>"
+//}
+
+//document.getElementById("tabell").innerHTML = html;
